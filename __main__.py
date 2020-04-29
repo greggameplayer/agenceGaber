05d79fbe5c70e21ffe9e049d3c4c89e0ae90b788
greggameplayer <gregoire.hage@gmail.com>
2020-04-29 11:18:40 +0200

    improve import pyodbc system

--- __main__.py
+++ __main__.py
@@ -1,6 +1,3 @@
 from agenceGaber.main import Main
-import os
 
-os.system('py -m pip install --upgrade pip')
-os.system('pip install pyodbc wheel')
 Main()
--- agenceGaber/main.py
+++ agenceGaber/main.py
@@ -1,5 +1,11 @@
-import pyodbc
+import os
 
+try:
+    import pyodbc
+except ImportError:
+    os.system('py -m pip install --upgrade pip')
+    os.system('pip install pyodbc wheel')
+    import pyodbc
 
 def ConnectToDatabase():
     conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:greggameplayer.database.windows.net,1433;Database=AgenceGaber;Uid=greggameplayer;Pwd=Gg123456;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
