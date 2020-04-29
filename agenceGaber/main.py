2119be131beb905412d77e2b4096944d7677613b
greggameplayer <gregoire.hage@gmail.com>
2020-04-29 10:54:39 +0200

    change odbc driver version

--- agenceGaber/main.py
+++ agenceGaber/main.py
@@ -2,7 +2,7 @@ import pyodbc
 
 
 def ConnectToDatabase():
-    conn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:greggameplayer.database.windows.net,1433;Database=AgenceGaber;Uid=greggameplayer;Pwd=Gg123456;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+    conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:greggameplayer.database.windows.net,1433;Database=AgenceGaber;Uid=greggameplayer;Pwd=Gg123456;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
     conn.setencoding(encoding='utf-8')
     return conn
 
